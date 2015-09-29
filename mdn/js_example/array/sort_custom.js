ab8dfe2ae19c4e3d9643425fcb4a484630ce3b3d
liuJinsong <zjj_ljs@163.com>
2015-09-29 09:42:30 +0800

    Added some examples

---
+++ mdn/js_example/array/sort_custom.js
@@ -0,0 +1,20 @@
+var items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic' },
+  { name: 'Zeros', value: 37 }
+];
+console.log(items);
+items.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  // a 和 b 的 name 相等
+  return 0;
+});
+console.log(items);
---
+++ mdn/js_example/array/sort_simple.js
@@ -0,0 +1,17 @@
+var fruit = ['apples', 'bananas', 'Cherries'];
+console.log (fruit);
+fruit.sort(); // ['Cherries', 'apples', 'bananas'];
+console.log (fruit);
+
+var scores = [1, 2, 10, 21];
+console.log (scores);
+scores.sort(); // [1, 10, 2, 21]
+console.log (scores);
+
+var things = ['word', 'Word', '1 Word', '2 Words'];
+console.log (things);
+things.sort(); // ['1 Word', '2 Words', 'Word', 'word']
+console.log (things);
+
+// In Unicode, numbers come before upper case letters,
+// which come before lower case letters.
