9291b3f599677a3359ce45fa10e1403bfe658e51
liuJinsong <zjj_ljs@163.com>
2016-01-22 18:28:58 +0800

    for monitor demo

--- mg-monitor/js/node-list-ctrl.js
+++ mg-monitor/js/node-list-ctrl.js
@@ -7,12 +7,22 @@ function($scope, monitorService, monitorResource){
         if (arguments.length > 0) {
             service.pageStat.nodeList.cluster = c;
         }
-        console.log(service.pageStat.nodeList.cluster);
+        //console.log(service.pageStat.nodeList.cluster);
         return service.pageStat.nodeList.cluster;
-    }
+    };
 
-    $scope.nodeList = monitorResource.query({name:"all"});
-    console.log(nodeList);
+    $scope.clusterChanged = function() {
+        console.log (service.pageStat.nodeList.cluster);
+        var name = "all";
+        if (service.pageStat.nodeList.cluster){
+            name = service.pageStat.nodeList.cluster.nodeData;
+        }
+        $scope.nodes=monitorResource.query({name:name});
+        console.log($scope.nodes);
+    };
+
+    $scope.clusterChanged();
+    //console.log($scope.nodes);
 
     $scope.orderTypes = [
         {name:"ip"},
@@ -26,7 +36,7 @@ function($scope, monitorService, monitorResource){
             service.pageStat.nodeList.order.type = t;
         }
         return service.pageStat.nodeList.order.type;
-    }
+    };
 
     $scope.orderUp = function () {
         return service.pageStat.nodeList.order.asc;
