fb0856e86ecc84dfd2b2ee8c786f4e204ffc91c9
zjjljs <zjj_ljs@qq.com>
2019-02-18 14:12:33 +0800

    2 net test , 1 dns test prog

---
+++ nodejs/tutorial-point/buffer-init.js
@@ -0,0 +1,8 @@
+var buf = Buffer.alloc(10);
+console.log(buf);
+var buf = Buffer.allocUnsafe(10);
+console.log(buf);
+var buf = Buffer.from([10, 20, 30, 40, 50]);
+console.log(buf);
+var buf = Buffer.from("Simply Easy Learning", "utf-8");
+console.log(buf);
