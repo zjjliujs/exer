3c52b51ecf8429e1b715705d6ff372b135abfc30
zjjljs <zjj_ljs@163.com>
2019-09-25 16:20:22 +0800

    CMake tutorial step3 sample code with unit test

---
+++ cmake/tutorial/step3_test/MathFunctions/MathFunctions.h
@@ -0,0 +1,7 @@
+// A simple program that computes the square root of a number
+#ifndef MY_SQRT
+#define MY_SQRT
+
+double mysqrt(double value);
+
+#endif
---
+++ cmake/tutorial/step3_test/MathFunctions/mysqrt.cxx
@@ -0,0 +1,9 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <math.h>
+#include "MathFunctions.h"
+
+double mysqrt(double value) {
+	printf("mysqrt entry!\n");
+	return sqrt(value);
+}
