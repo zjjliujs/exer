1d6667e60c59f6732b3b5032a3d9b63a0654745b
zjjljs <zjj_ljs@163.com>
2020-07-22 16:08:14 +0800

    add protobuffer test

---
+++ protobuffer/hello-world/cpp/reader.cpp
@@ -0,0 +1,25 @@
+#include "helloworld.pb.h" 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void ListMsg(const lm::helloworld & msg) { 
+	cout << msg.id() << endl; 
+	cout << msg.str() << endl; 
+} 
+
+int main(int argc, char* argv[]) { 
+
+	lm::helloworld msg1; 
+
+	{ 
+		fstream input("./log", ios::in | ios::binary); 
+		if (!msg1.ParseFromIstream(&input)) { 
+			cerr << "Failed to parse address book." << endl; 
+			return -1; 
+		} 
+	} 
+
+	ListMsg(msg1); 
+}
