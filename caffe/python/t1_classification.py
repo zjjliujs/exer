990d199f49c0aee1ba786ebdf4fdd341450529e1
zjjljs <zjj_ljs@163.com>
2020-07-27 13:46:20 +0800

    update

--- caffe/python/t1_classification.py
+++ caffe/python/t1_classification.py
@@ -1,13 +1,13 @@
 # set up Python environment: numpy for numerical routines, and matplotlib for plotting
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 # display plots in this notebook
 # %matplotlib inline
 
 # set display defaults
-plt.rcParams['figure.figsize'] = (10, 10)        # large images
-plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show square pixels
-plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
+#plt.rcParams['figure.figsize'] = (10, 10)        # large images
+#plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show square pixels
+#plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
 
 # The caffe module needs to be on the Python path;
 #  we'll add it here explicitly.
