48839ad677691862348bcf58c80f272b22e9990a
zjjljs <zjj_ljs@163.com>
2020-07-27 15:59:40 +0800

    update

--- caffe/python/mnist/train_2.py
+++ caffe/python/mnist/train_2.py
@@ -6,11 +6,11 @@ caffe.set_mode_cpu()
 solver = caffe.SGDSolver('examples/mnist/lenet_solver.prototxt')
 #solver.solve()
 
-iter = solver.iter
-while iter<10000:
+it = solver.iter
+while it<10000:
     solver.step(1)
-    iter = solver.iter
+    it = solver.iter
     input_data = solver.net.blobs['data'].data
     loss = solver.net.blobs['loss'].data
     accuracy = solver.test_nets[0].blobs['accuracy'].data
-    print 'iter:', iter, 'loss:', loss,'accuracy:',accuracy
+    print ('iter: %d loss: %f, accuracy:%d', it, loss, accuracy)
