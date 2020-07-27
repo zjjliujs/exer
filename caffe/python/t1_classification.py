fd87134be33e9edba0201e6f929eb04986b267ca
zjjljs <zjj_ljs@163.com>
2020-07-27 10:28:18 +0800

    add

---
+++ caffe/python/t1_classification.py
@@ -0,0 +1,19 @@
+# set up Python environment: numpy for numerical routines, and matplotlib for plotting
+import numpy as np
+import matplotlib.pyplot as plt
+# display plots in this notebook
+# %matplotlib inline
+
+# set display defaults
+plt.rcParams['figure.figsize'] = (10, 10)        # large images
+plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show square pixels
+plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
+
+# The caffe module needs to be on the Python path;
+#  we'll add it here explicitly.
+import sys
+caffe_root = '../'  # this file should be run from {caffe_root}/examples (otherwise change this line)
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+# If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
