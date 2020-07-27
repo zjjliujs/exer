b769fb0c8d6e696e00ae32016e83fcc81ef4b368
zjjljs <zjj_ljs@163.com>
2020-07-27 17:08:17 +0800

    update

--- caffe/python/t1_classification.py
+++ caffe/python/t1_classification.py
@@ -39,4 +39,53 @@ net = caffe.Net(model_def,      # defines the structure of the model
                 model_weights,  # contains the trained weights
                 caffe.TEST)     # use test mode (e.g., don't perform dropout)
 
+# load the mean ImageNet image (as distributed with Caffe) for subtraction
+mu = np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
+mu = mu.mean(1).mean(1)  # average over pixels to obtain the mean (BGR) pixel values
+print 'mean-subtracted values:', zip('BGR', mu)
+
+# create transformer for the input called 'data'
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+
+transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
+transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
+transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
+transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
+
+# set the size of the input (we can skip this if we're happy
+#  with the default; we can also change it later, e.g., for different batch sizes)
+net.blobs['data'].reshape(50,        # batch size
+                          3,         # 3-channel (BGR) images
+                          227, 227)  # image size is 227x227
+
+image = caffe.io.load_image(caffe_root + 'examples/images/cat.jpg')
+transformed_image = transformer.preprocess('data', image)
+plt.imshow(image)
+
+# copy the image data into the memory allocated for the net
+net.blobs['data'].data[...] = transformed_image
+
+### perform classification
+output = net.forward()
+
+output_prob = output['prob'][0]  # the output probability vector for the first image in the batch
+
+print('predicted class is: %f' % output_prob.argmax())
+
+# load ImageNet labels
+labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt'
+if not os.path.exists(labels_file):
+    !../data/ilsvrc12/get_ilsvrc_aux.sh
+
+labels = np.loadtxt(labels_file, str, delimiter='\t')
+
+print ('output label: %f' % labels[output_prob.argmax()])
+
+# sort top five predictions from softmax output
+top_inds = output_prob.argsort()[::-1][:5]  # reverse sort and take five largest items
+
+print 'probabilities and labels:'
+zip(output_prob[top_inds], labels[top_inds])
+
+
 
