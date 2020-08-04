4409e963302cfdd33a0e6ac4eacaa668ad3b92e1
zjjljs <zjj_ljs@163.com>
2020-08-04 09:02:25 +0800

    generator sample code

---
+++ python/lang/generator_test.py
@@ -0,0 +1,8 @@
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        print("index:{}".format(index))
+        yield data[index]
+
+if __name__=="__main__":
+    for char in reverse('golf'):
+        print(char)
---
+++ python/lang/iter_test.py
@@ -0,0 +1,19 @@
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+
+r = Reverse([1,2,3,4,5,6,7])
+for i in r:
+    print (i)
